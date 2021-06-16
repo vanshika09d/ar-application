1761232aa220cae79cbc4a389b6dfd47deaed0c6
vanshika09d <vanshikadeshpande99@gmail.com>
2021-06-16 23:32:01 +0530

    AR Application

---
+++ app/src/main/java/com/wheic/myapplication/EducationActivity.java
@@ -0,0 +1,34 @@
+package com.wheic.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class EducationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_education);
+
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home){
+            Intent categoryIntent = new Intent(EducationActivity.this,HomeActivity.class);
+            startActivity(categoryIntent);
+            finish();
+
+            }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/wheic/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.wheic.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+        imageView.startAnimation(animation);
+
+
+        Thread timer = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    sleep(8000);
+                    Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+        timer.start();
+    }
+}
